2ba46f57eaa7965ebd2aa19f02fc6e1422c5a537
Gabriel Rodriguez <gabrielroquerodriguez@gmail.com>
2023-02-19 22:47:52 -0800

    Add user input that decides grid size

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
+// store height and width
+let gridHeightAndWidth = ''
+
 // css grid container
 const container = document.querySelector(".container")
 
 // create child divs and assign class
-function createDivs (num) {
-    for (let i = 0; i <= num; i++) {
+function createDivs(num) {
+    // square height and width in for loop
+    for (let i = 0; i <= (Math.pow(gridHeightAndWidth, 2) -1); i++) {
         const div = document.createElement('div'); //rename?
         div.classList.add('grid-item');
         container.appendChild(div);
 
-        // change color over div when cursor hovers
+        // change color of grid item when cursor hovers over it
         div.addEventListener('mouseenter', () => {
-            div.classList.add('grid-hover-change');})
+            div.classList.add('grid-hover-change');
+        })
     }
 }
 
-// add popup button asking input for number of grid rows/columns
+// get height and width from user and assign to style
+document.getElementById("height-and-width").onclick = () => getHeight();
+
+function getHeight() {
+    gridHeightAndWidth = prompt("Enter grid height", 16);
+    document.documentElement.style.setProperty('--height-and-width', gridHeightAndWidth);
+    createDivs();
+}
